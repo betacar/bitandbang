02a30ed90231dd429a72e3d8b41a84d78af76e29
Carlos Betancourt Carrero <carbetacar@gmail.com>
2018-12-21 10:44:23 -0300

    Personalized!

--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Carlos Betancourt Carrero /'),
+  handle: chalk.cyan('betacar'),
+  work: chalk.white('Director of Product Engineering at uBiome'),
+  twitter: chalk.cyan('https://twitter.com/betacar'),
+  github: chalk.cyan('https://github.com/betacar'),
+  linkedin: chalk.cyan('https://linkedin.com/in/betacar'),
+  web: chalk.cyan('https://betacar.net'),
+  npx: chalk.white('npx betacar'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
